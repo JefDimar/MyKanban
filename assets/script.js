3b6890b90851da7f275dcc483677ba097e757ce9
jefDimar <jefdimar@gmail.com>
2021-02-10 18:24:40 +0700

    done register login

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,108 @@
 const app = new Vue({
   el: "#app",
   data: {
-    page: "login",
-    email: '',
-    password: '',
-    backlog: '',
-    doing: '',
-    todo: '',
-    done: '',
-    category: '',
-    tasks: []
+    page: "",
+    login: {
+      email: "",
+      password: "",
+    },
+    register: {
+      email: "",
+      password: "",
+    },
+    backlog: "",
+    doing: "",
+    todo: "",
+    done: "",
+    category: "",
+    tasks: [],
+    server: "http://localhost:3030/",
   },
   methods: {
     changePage(page) {
-      this.page = page
+      this.page = page;
     },
-    login() {
-      console.log(this.email);
-      console.log(this.password);
+    loginServ() {
+      axios({
+        method: "POST",
+        url: this.server + "login",
+        data: {
+          email: this.login.email,
+          password: this.login.password,
+        },
+      })
+        .then(({ data }) => {
+          localStorage.setItem("access_token", data.access_token);
+          this.page = "main";
+        })
+        .catch(({ response }) => {
+          const errors = response.data.message;
+          Swal.fire(errors, "", "error");
+          this.page = "login";
+        })
+        .then((_) => {
+          this.login.email = "";
+          this.login.password = "";
+        });
     },
-    register() {
-      console.log(this.email);
-      console.log(this.password);
+    registerServ() {
+      axios({
+        method: "POST",
+        url: this.server + "register",
+        data: {
+          email: this.register.email,
+          password: this.register.password,
+        },
+      })
+        .then(({ data }) => {
+          console.log(data);
+          this.page = "login";
+        })
+        .catch(({ response }) => {
+          const errors = response.data.message;
+          if (Array.isArray(errors)) {
+            Swal.fire(errors.join("\n"), "", "error");
+          } else {
+            Swal.fire(errors, "", "error");
+          }
+          this.page = "register";
+        })
+        .then((_) => {
+          this.register.email = "";
+          this.register.password = "";
+        });
     },
-    createTasks(category) {
-      this.category = category;
-      console.log(this.backlog);
-      console.log(this.doing);
-      console.log(this.todo);
-      console.log(this.done);
-      console.log(category);
+    btnLogout() {
+      localStorage.clear();
+      this.page = "login";
+    },
+    fetchTasks() {
+      axios({
+        method: "GET",
+        url: this.server + "tasks",
+        headers: {
+          access_token: localStorage.access_token,
+        },
+      })
+        .then(({ data }) => {
+          console.log(data);
+          this.tasks.push(data);
+        })
+        .catch(({ response }) => {
+          console.log(response.data);
+        });
+    },
+    createTasks() {
+      
+    },
+  },
+  created() {
+    const access_token = localStorage.access_token;
+    if (access_token) {
+      this.page = "main";
+      this.fetchTasks;
+    } else {
+      this.page = "login";
     }
   },
 });
