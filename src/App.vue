3f4f72c44522dee9c1b41eeef1ba84e833d75bff
jefDimar <jefdimar@gmail.com>
2021-02-11 11:50:39 +0700

    web structure init

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
+<div>
   <h1>{{ message }}</h1>
+</div>
 </template>
 
 <script>
+import Navbar from './views/Navbar'
+import Login from './views/Login'
+import Register from './views/Register'
+import Main from './views/Main'
+
 export default {
   data() {
     return {
